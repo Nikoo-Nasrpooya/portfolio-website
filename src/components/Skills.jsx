7dc549df5b6d04c3672162cccbc89eafeda1b56f
Nikoo <nikoonasrpooya@yahoo.com>
2022-11-28 01:29:28 -0600

    update skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,6 +8,11 @@ import nextjs from "../assets/nextjs.png";
 import graphql from "../assets/graphql.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
+import bootstrap from "../assets/bootstrap.png"
+import git from "../assets/git.png"
+import visualstudio from "../assets/visualstudio.png"
+import python from "../assets/python.png"
+import nodejs from "../assets/node.png"
 
 const Skills = () => {
   const techs = [
@@ -15,49 +20,66 @@ const Skills = () => {
       id: 1,
       src: html,
       title: "HTML",
-      style: "shadow-orange-500",
     },
     {
       id: 2,
       src: css,
       title: "CSS",
-      style: "shadow-blue-500",
     },
     {
       id: 3,
       src: javascript,
       title: "JavaScript",
-      style: "shadow-yellow-500",
     },
     {
       id: 4,
       src: reactImage,
       title: "React",
-      style: "shadow-blue-600",
     },
     {
       id: 5,
       src: tailwind,
       title: "Tailwind",
-      style: "shadow-sky-400",
     },
     {
       id: 6,
       src: nextjs,
       title: "Next JS",
-      style: "shadow-white",
     },
     {
       id: 7,
       src: graphql,
       title: "GraphQL",
-      style: "shadow-pink-400",
     },
     {
       id: 8,
       src: github,
       title: "GitHub",
-      style: "shadow-gray-400",
+    },
+    {
+      id: 9,
+      src: bootstrap,
+      title: "Bootstrap",
+    },
+    {
+      id: 10,
+      src: git,
+      title: "Git",
+    },
+    {
+      id: 11,
+      src: visualstudio,
+      title: "Visual Studio",
+    },
+    {
+      id: 12,
+      src: python,
+      title: "Python",
+    },
+    {
+      id: 13,
+      src: nodejs,
+      title: "Node Js",
     },
   ];
 
@@ -74,11 +96,11 @@ const Skills = () => {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {techs.map(({ id, src, title, style }) => (
+        <div className="w-full grid grid-cols-3 sm:grid-cols-4 gap-6 text-center py-8 px-12 sm:px-0">
+          {techs.map(({ id, src, title }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              className="hover:scale-125 duration-500 py-2 rounded-lg"
             >
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
