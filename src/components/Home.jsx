f2ddc16dd42be6821520d47a70bf99b8aa75d397
Nikoo <nikoonasrpooya@yahoo.com>
2022-11-28 12:33:22 -0600

    remove the white color of h1 in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name="home" className="h-screen w-full bg-">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-5xl font-bold text-white ">
+          <h2 className="text-4xl sm:text-5xl font-bold">
             I'm a Front-end Web Developer
           </h2>
           <p className=" py-4 max-w-md">
